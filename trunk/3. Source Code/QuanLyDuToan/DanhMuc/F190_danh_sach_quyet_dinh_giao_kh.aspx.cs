f1aee55245c47b11808552fa4a7efd4aabe1236b
minhtu.bkhn@gmail.com <minhtu.bkhn@gmail.com@ebe17c50-ee7f-84cb-8695-e18aa4d95bf0>
2015-03-11 08:47:35 +0000

    Refact F190

--- trunk/3. Source Code/QuanLyDuToan/DanhMuc/F190_danh_sach_quyet_dinh_giao_kh.aspx.cs
+++ trunk/3. Source Code/QuanLyDuToan/DanhMuc/F190_danh_sach_quyet_dinh_giao_kh.aspx.cs
@@ -19,8 +19,6 @@ namespace QuanLyDuToan.DanhMuc
 {
 	public partial class F190_danh_sach_quyet_dinh_giao_kh : System.Web.UI.Page
 	{
-        US_V_DM_QUYET_DINH_KH m_us = new US_V_DM_QUYET_DINH_KH();
-
 		#region Public Functions
 		public bool isEnableSelectDropdownlist(DropDownList ip_ddl)
 		{
@@ -30,8 +28,45 @@ namespace QuanLyDuToan.DanhMuc
 		}
 		#endregion
 
+		#region Data Structures
+		
+		#endregion
+
+		#region Members
+		US_V_DM_QUYET_DINH_KH m_us = new US_V_DM_QUYET_DINH_KH();
+
+		#endregion
+
 		#region Private Methods
-		private bool check_validate_data_is_ok()
+		private void set_initial_form_load()
+		{
+			//1. Lấy dữ liệu ngày tháng từ query string
+			m_txt_tu_ngay.Text = WebformFunctions.getValue_from_query_string<string>(
+														this
+														, FormInfo.QueryString.TU_NGAY
+														, CIPConvert.ToStr(CCommonFunction.getDate_dau_nam_from_date(DateTime.Now), "dd/MM/yyyy")
+														);
+			m_txt_den_ngay.Text = WebformFunctions.getValue_from_query_string<string>(
+														this
+														, FormInfo.QueryString.DEN_NGAY
+														, CIPConvert.ToStr(CCommonFunction.getDate_cuoi_nam_form_date(DateTime.Now), "dd/MM/yyyy")
+														);
+			//2. Đổ dữ liệu vào dropdownlist Đơn vị
+			WebformControls.load_data_to_ddl_don_vi_get_list_don_vi_duoc_xem_du_lieu(
+								Person.get_id_don_vi()
+								, m_ddl_don_vi
+								);
+			m_ddl_don_vi.SelectedValue = WebformFunctions.getValue_from_query_string<string>(
+																this
+																, FormInfo.QueryString.ID_DON_VI
+																, Person.get_id_don_vi().ToString()
+																);
+
+			
+			//3. Đổ dữ liệu lên lưới
+			load_data_to_grid();
+		}
+		private bool check_validate_input_report()
 		{
 			if (!CValidateTextBox.IsValid(m_txt_tu_ngay, DataType.DateType, allowNull.NO))
 			{
@@ -47,91 +82,35 @@ namespace QuanLyDuToan.DanhMuc
 			}
 			return true;
 		}
-
 		private void load_data_to_grid()
 		{
 			m_lbl_info.Text = "";
-			if (!check_validate_data_is_ok()) return;
-
-			DateTime v_dat_tu_ngay = CIPConvert.ToDatetime(m_txt_tu_ngay.Text, "dd/MM/yyyy");
-			DateTime v_dat_den_ngay = CIPConvert.ToDatetime(m_txt_den_ngay.Text, "dd/MM/yyyy");
-
-			decimal v_dc_id_loai_nhiem_vu = -1, v_dc_id_id_cong_trinh = -1, v_dc_id_du_an = -1;
-
-			if (Request.QueryString["ip_dc_id_loai_nhiem_vu"] != null)
-			{
-				v_dc_id_loai_nhiem_vu = CIPConvert.ToDecimal(Request.QueryString["ip_dc_id_loai_nhiem_vu"]);
-				if (v_dc_id_loai_nhiem_vu != -1)
-				{
-					US_CM_DM_TU_DIEN v_us_loai_nhiem_vu = new US_CM_DM_TU_DIEN(v_dc_id_loai_nhiem_vu);
-					m_lbl_info.Text = "Loại nhiệm vụ: " + v_us_loai_nhiem_vu.strTEN;
-				}
-
-			}
-			if (Request.QueryString["ip_dc_id_cong_trinh"] != null)
-			{
-				v_dc_id_id_cong_trinh = CIPConvert.ToDecimal(Request.QueryString["ip_dc_id_cong_trinh"]);
-				if (v_dc_id_id_cong_trinh != -1)
-				{
-					US_DM_CONG_TRINH_DU_AN_GOI_THAU v_us_cong_trinh = new US_DM_CONG_TRINH_DU_AN_GOI_THAU(v_dc_id_id_cong_trinh);
-					m_lbl_info.Text += "- Công trình/Quốc lộ: " + v_us_cong_trinh.strTEN;
-				}
-			}
-			if (Request.QueryString["ip_dc_id_du_an"] != null)
-			{
-				v_dc_id_du_an = CIPConvert.ToDecimal(Request.QueryString["ip_dc_id_du_an"]);
-				if (v_dc_id_id_cong_trinh != -1)
-				{
-					US_DM_CONG_TRINH_DU_AN_GOI_THAU v_us_du_an = new US_DM_CONG_TRINH_DU_AN_GOI_THAU(v_dc_id_du_an);
-					m_lbl_info.Text += "- Dự án: " + v_us_du_an.strTEN;
-				}
-			}
-            US_V_DM_QUYET_DINH_KH v_us = new US_V_DM_QUYET_DINH_KH();
-            DS_V_DM_QUYET_DINH_KH v_ds = new DS_V_DM_QUYET_DINH_KH();
-            v_ds.EnforceConstraints = false;
-            v_us.FillDatasetGiaoKHByIdDonVi(
-                v_ds,
-                CIPConvert.ToDecimal(m_ddl_don_vi.SelectedValue),
-                CIPConvert.ToDatetime(m_txt_tu_ngay.Text, c_configuration.DEFAULT_DATETIME_FORMAT),
-                CIPConvert.ToDatetime(m_txt_den_ngay.Text, c_configuration.DEFAULT_DATETIME_FORMAT),
-                m_txt_tu_khoa_tim_kiem.Text
-                );
-            m_grv_bao_cao_giao_von.DataSource = v_ds.V_DM_QUYET_DINH_KH;
+			if (!check_validate_input_report()) return;
+			US_V_DM_QUYET_DINH_KH v_us = new US_V_DM_QUYET_DINH_KH();
+			DS_V_DM_QUYET_DINH_KH v_ds = new DS_V_DM_QUYET_DINH_KH();
+			v_ds.EnforceConstraints = false;
+			v_us.FillDatasetGiaoKHByIdDonVi(
+					v_ds,
+					CIPConvert.ToDecimal(m_ddl_don_vi.SelectedValue),
+					CIPConvert.ToDatetime(m_txt_tu_ngay.Text, c_configuration.DEFAULT_DATETIME_FORMAT),
+					CIPConvert.ToDatetime(m_txt_den_ngay.Text, c_configuration.DEFAULT_DATETIME_FORMAT),
+					m_txt_tu_khoa_tim_kiem.Text.Trim()
+					);
+			m_grv_bao_cao_giao_von.DataSource = v_ds.V_DM_QUYET_DINH_KH;
 			m_grv_bao_cao_giao_von.DataBind();
-
 		}
-
 		private void export_excel()
 		{
 			US_DM_DON_VI v_us = new US_DM_DON_VI(CIPConvert.ToDecimal(m_ddl_don_vi.SelectedValue));
 			WebformReport.export_gridview_2_excel(
-			m_grv_bao_cao_giao_von, "[" + v_us.strTEN_DON_VI + "]BaoCaoGiaoKeHoachTheoQuyetDinh.xls");
+							m_grv_bao_cao_giao_von
+							, "[" + v_us.strTEN_DON_VI + "]" + FormInfo.ExportExcelReportName.F190
+							);
 		}
-
-        private void us_2_form(){
-            m_txt_noi_dung.Text = m_us.strNOI_DUNG;
-            m_txt_so_quyet_dinh.Text = m_us.strSO_QUYET_DINH;
-            m_txt_ngay_thang.Text = CIPConvert.ToStr(m_us.datNGAY_THANG, c_configuration.DEFAULT_DATETIME_FORMAT);
-            m_ddl_loai_quyet_dinh_giao.SelectedValue = m_us.dcID_LOAI_QUYET_DINH_GIAO.ToString();
-        }
-
-        private void form_2_us() {
-            m_us.strNOI_DUNG = m_txt_noi_dung.Text;
-            m_us.strSO_QUYET_DINH = m_txt_so_quyet_dinh.Text;
-            m_us.datNGAY_THANG = CIPConvert.ToDatetime(m_txt_ngay_thang.Text, c_configuration.DEFAULT_DATETIME_FORMAT);
-            m_us.dcID_LOAI_QUYET_DINH_GIAO = CIPConvert.ToDecimal(m_ddl_loai_quyet_dinh_giao.SelectedValue);
-            m_us.dcID_LOAI_QUYET_DINH = 647;//id giao ke hoach
-        }
-
-        private void load_data_2_ddl_loai_quyet_dinh_giao() {
-            US_CM_DM_TU_DIEN v_us = new US_CM_DM_TU_DIEN();
-            DS_CM_DM_TU_DIEN v_ds = new DS_CM_DM_TU_DIEN();
-            v_us.fill_tu_dien_cung_loai_ds("LOAI_GIAO_DICH", v_ds);
-            m_ddl_loai_quyet_dinh_giao.DataSource = v_ds.CM_DM_TU_DIEN;
-            m_ddl_loai_quyet_dinh_giao.DataValueField = CM_DM_TU_DIEN.ID;
-            m_ddl_loai_quyet_dinh_giao.DataTextField = CM_DM_TU_DIEN.TEN;
-            m_ddl_loai_quyet_dinh_giao.DataBind();
-        }
+		public override void VerifyRenderingInServerForm(Control control)
+		{
+			//base.VerifyRenderingInServerForm(control);
+		}
 
 		#endregion
 
@@ -140,31 +119,11 @@ namespace QuanLyDuToan.DanhMuc
 		{
 			try
 			{
+				m_lbl_info.Text = "";
+				m_lbl_info.Text = "";
 				if (!IsPostBack)
 				{
-                    if (Request.QueryString["ip_dat_tu_ngay"] != null)
-                    {
-                        m_txt_tu_ngay.Text = Request.QueryString["ip_dat_tu_ngay"].ToString();
-                    }
-                    else
-                    {
-                        m_txt_tu_ngay.Text = CIPConvert.ToStr(WebformControls.get_dau_nam_form_date(DateTime.Now), "dd/MM/yyyy");
-                    }
-                    if (Request.QueryString["ip_dat_den_ngay"] != null)
-                    {
-                        m_txt_den_ngay.Text = Request.QueryString["ip_dat_den_ngay"].ToString();
-                    }
-                    else
-                    {
-                        m_txt_den_ngay.Text = CIPConvert.ToStr(DateTime.Now, "dd/MM/yyyy");
-                    }
-                    if (Request.QueryString["ip_dc_id_don_vi"] != null)
-                    {
-                        m_ddl_don_vi.SelectedValue = Request.QueryString["ip_dc_id_don_vi"].ToString();
-                    }
-                    WebformControls.load_data_to_ddl_don_vi_get_list_don_vi_duoc_xem_du_lieu(Person.get_id_don_vi(), m_ddl_don_vi);
-                    load_data_2_ddl_loai_quyet_dinh_giao();
-					load_data_to_grid();
+					set_initial_form_load();
 				}
 			}
 			catch (Exception v_e)
@@ -172,6 +131,7 @@ namespace QuanLyDuToan.DanhMuc
 				CSystemLog_301.ExceptionHandle(this, v_e);
 			}
 		}
+
 		protected void m_cmd_tim_kiem_Click(object sender, EventArgs e)
 		{
 			try
@@ -194,49 +154,19 @@ namespace QuanLyDuToan.DanhMuc
 				CSystemLog_301.ExceptionHandle(this, v_e);
 			}
 		}
-        protected void m_ddl_don_vi_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                load_data_to_grid();
-            }
-            catch (Exception v_e)
-            {
-                CSystemLog_301.ExceptionHandle(this, v_e);
-            }
-        }
+
+		protected void m_ddl_don_vi_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				load_data_to_grid();
+			}
+			catch (Exception v_e)
+			{
+				CSystemLog_301.ExceptionHandle(this, v_e);
+			}
+		}
 		#endregion
 
-        protected void m_cmd_insert_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                form_2_us();
-                if (check_us())
-                {
-                    m_us.Insert();
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-		public override void VerifyRenderingInServerForm(Control control)
-		{
-			//base.VerifyRenderingInServerForm(control);
-		}
-        private bool check_us()
-        {
-            if (m_us.strSO_QUYET_DINH.Trim() == "")
-            {
-                return false;
-            }
-            if (m_us.strNOI_DUNG.Trim() == "")
-            {
-                return false;
-            }
-            return true;
-        }
 	}
 }
--- trunk/3. Source Code/WebsiteUS/FormInfo.cs
+++ trunk/3. Source Code/WebsiteUS/FormInfo.cs
@@ -6,26 +6,26 @@ namespace WebUS
 {
 	public class FormInfo
 	{
-		
+
 		public class QueryString
 		{
 			public const string ID_DON_VI = "ip_dc_id_don_vi";
 			public const string TU_NGAY = "ip_dat_tu_ngay";
 			public const string DEN_NGAY = "ip_dat_den_ngay";
 			public const string ID_LOAI_NHIEM_VU = "ip_dc_id_loai_nhiem_vu";
-            public const string ID_CONG_TRINH = " ip_dc_id_cong_trinh";
-            public const string ID_DU_AN = "ip_dc_id_du_an";
-            public const string NGUON_NGAN_SACH = "ip_nguon_ns";
+			public const string ID_CONG_TRINH = " ip_dc_id_cong_trinh";
+			public const string ID_DU_AN = "ip_dc_id_du_an";
+			public const string NGUON_NGAN_SACH = "ip_nguon_ns";
 			public const string ID_GIAI_NGAN = "ip_dc_id_dm_giai_ngan";
-            public static string ID_QUYET_DINH = "ip_dc_id_quyet_dinh";
+			public static string ID_QUYET_DINH = "ip_dc_id_quyet_dinh";
 		}
 
 		public class FormName
 		{
 			public const string F350 = "F350_tinh_hinh_giai_ngan.aspx";
-            public const string F157 = "F157_giao_ke_hoach_theo_quyet_dinh.aspx";
-            public const string F257 = "F257_giao_von_theo_quyet_dinh.aspx";
-            public const string F357 = "F357_giai_ngan_theo_uy_nhiem_chi.aspx";
+			public const string F157 = "F157_giao_ke_hoach_theo_quyet_dinh.aspx";
+			public const string F257 = "F257_giao_von_theo_quyet_dinh.aspx";
+			public const string F357 = "F357_giai_ngan_theo_uy_nhiem_chi.aspx";
 		}
 
 		public class FormRedirect
@@ -37,10 +37,12 @@ namespace WebUS
 		public class ExportExcelReportName
 		{
 			public const string F530 = "BaoCaoTinhHinhGiaiNganCacDonVi.xls";
-            public const string F350 = "BaoCaoTinhHinhGiaiNgan.xls";
-            public const string F157 = "BaoCaoTinhHinhGiaoKeHoach.xls";
+			public const string F350 = "BaoCaoTinhHinhGiaiNgan.xls";
+			public const string F157 = "BaoCaoTinhHinhGiaoKeHoach.xls";
 			public const string F257 = "BaoCaoTinhHinhGiaoVon.xls";
 			public const string F357 = "BaoCaoTinhHinhGiaiNgan.xls";
-        }
+			public const string F190 = "BaoCaoGiaoKeHoachTheoQuyetDinh.xls";
+
+		}
 	}
 }
